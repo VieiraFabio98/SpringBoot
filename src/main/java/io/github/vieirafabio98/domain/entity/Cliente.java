16b96be13bbfcf9eb1741bcf33a4dc7df1bdd4c1
VieiraFabio98 <vieirafabio5271@outlook.com>
2023-09-21 10:07:10 -0300

    mapeando o resto das entitidades.

--- src/main/java/io/github/vieirafabio98/domain/entity/Cliente.java
+++ src/main/java/io/github/vieirafabio98/domain/entity/Cliente.java
@@ -1,6 +1,7 @@
 package io.github.vieirafabio98.domain.entity;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table
@@ -14,8 +15,19 @@ public class Cliente {
     @Column(name = "nome", length = 100)
     private String nome;
 
+    @OneToMany(mappedBy = "cliente")
+    private Set<Pedido> pedidos;
+
     public Cliente() {
 
+    }
+
+    public Set<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(Set<Pedido> pedidos) {
+        this.pedidos = pedidos;
     }
 
     public Cliente(String nome) {

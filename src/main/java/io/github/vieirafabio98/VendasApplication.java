ca1ac0786f4e3c3c675bf7c648dad081596eeb14
VieiraFabio98 <vieirafabio5271@outlook.com>
2023-09-21 15:32:14 -0300

    Finalizando seção 3, persistência e acesso a dados com Spring Data JPA.

--- src/main/java/io/github/vieirafabio98/VendasApplication.java
+++ src/main/java/io/github/vieirafabio98/VendasApplication.java
@@ -1,27 +1,43 @@
 package io.github.vieirafabio98;
 
 import io.github.vieirafabio98.domain.entity.Cliente;
+import io.github.vieirafabio98.domain.entity.Pedido;
 import io.github.vieirafabio98.domain.repository.Clientes;
+import io.github.vieirafabio98.domain.repository.Pedidos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
 
     @Bean
-    public CommandLineRunner init(@Autowired Clientes clientes) {
+    public CommandLineRunner init(
+            @Autowired Clientes clientes,
+            @Autowired Pedidos pedidos) {
         return args -> {
             System.out.println("Salvando Clientes");
-            clientes.save(new Cliente("Fabio"));
-            clientes.save(new Cliente("Marcelly"));
+            Cliente marcelly = new Cliente("Marcelly");
+            clientes.save(marcelly);
 
-            List<Cliente> result = clientes.encontrarPorNome("Marcelly");
-            result.forEach(System.out::println);
+            Pedido p = new Pedido();
+            p.setCliente(marcelly);
+            p.setDataPedido(LocalDate.now());
+            p.setTotal(BigDecimal.valueOf(100));
+
+            pedidos.save(p);
+
+//            Cliente cliente = clientes.findClienteFetchPedidos(marcelly.getId());
+//            System.out.println(cliente);
+//            System.out.println(cliente.getPedidos());
+
+            pedidos.findByCliente(marcelly).forEach((System.out::println));
 
 
         };

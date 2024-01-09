62d541ebdcc8100db43315e7ef3f574533a1530e
VieiraFabio98 <vieirafabio5271@outlook.com>
2024-01-08 23:35:40 -0300

    Get com filtro

--- src/main/java/io/github/vieirafabio98/api/controller/ClienteController.java
+++ src/main/java/io/github/vieirafabio98/api/controller/ClienteController.java
@@ -2,10 +2,13 @@ package io.github.vieirafabio98.api.controller;
 
 import io.github.vieirafabio98.domain.entity.Cliente;
 import io.github.vieirafabio98.domain.repository.Clientes;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -66,5 +69,16 @@ public class ClienteController {
                     clientes.save(cliente);
                     return ResponseEntity.noContent().build();
                 }).orElseGet( () -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/api/clientes")
+    public ResponseEntity find(Cliente filtro){
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+        Example example = Example.of(filtro, matcher);
+        List<Clientes> lista = clientes.findAll(example);
+        return ResponseEntity.ok(lista);
     }
 }
